5bfa80cbe9f6faee27862f787b9ba62b980efa03
Jacob-R96 <127170236+Jacob-R96@users.noreply.github.com>
2023-07-12 11:19:55 -0500

    Update settings.py

--- Wedding/settings.py
+++ Wedding/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$ay+8(r076_u$-uv76^h7m080ebq@&*vvrvmd36*ko+mb_r0^q'
+SECRET_KEY = '*******************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
